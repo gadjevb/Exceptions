e1e0984d95425dfc0762e37abbf9b24654aacd74
Borislav Gadjev <gadjevb@gmail.com>
2016-06-05 15:53:55 +0300

    The first task is completed!

--- src/com/clouway/exceptions/Demo.java
+++ src/com/clouway/exceptions/Demo.java
@@ -1,5 +1,16 @@
 package com.clouway.exceptions;
 
-public class Demo {
+import java.util.Scanner;
 
+public class Demo {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        Sumator sumator = new Sumator();
+        System.out.println("Please Enter the numbers(integers) you wish to sum: ");
+        System.out.print("First number: ");
+        String a = sc.nextLine();
+        System.out.print("Second number: ");
+        String b = sc.nextLine();
+        sumator.sum(a,b);
+    }
 }
---
+++ src/com/clouway/exceptions/Sumator.java
@@ -0,0 +1,42 @@
+package com.clouway.exceptions;
+
+public class Sumator {
+    protected void sum(String a, String b){
+        int numberA = 0, numberB = 0;
+        boolean flag = true;
+        boolean check = true;
+        try{
+            numberA = Integer.parseInt(a);
+        }catch (NumberFormatException ex){
+            for(int i = 0; i < a.length(); i++){
+                if(a.charAt(i) < 48 || a.charAt(i) > 57){
+                    System.out.println("In the first number, at position " + (i+1) + " you have entered (" + a.charAt(i) + ") instead of number!");
+                    flag = false;
+                    check = false;
+                }
+            }
+            if(check){
+                flag = false;
+            }
+        }
+        try{
+            numberB = Integer.parseInt(b);
+        }catch (NumberFormatException ex){
+            for(int i = 0; i < b.length(); i++){
+                if(b.charAt(i) < 48 || b.charAt(i) > 57){
+                    System.out.println("In the second number, at position " + (i+1) + " you have entered (" + b.charAt(i) + ") instead of number!");
+                    flag = false;
+                    check = false;
+                }
+            }
+            if(check){
+                flag = false;
+            }
+        }
+        if(flag == true && check == true){
+            System.out.print("The sum is: " + (numberA + numberB));
+        }else if(flag == false && check == true){
+            System.out.print("The value you entered surpasses the integer range of -2,147,483,648 to 2,147,483,647 or you have entered an empty line!");
+        }
+    }
+}
